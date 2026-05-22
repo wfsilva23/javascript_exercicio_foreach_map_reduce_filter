e8f4a0f0357ccd7768c83f93e20d6eb8e6c243ed
wfsilva23 <wfsilva23@gmail.com>
2026-05-22 17:42:27 -0300

    fix: ajusta case-sensitivity da pasta Assets

--- products.js
+++ products.js
@@ -1,15 +1,15 @@
 const menuOptions = [
 
-{ name: 'X-Salada', price: 30, vegan: false, src: './assets/xsalada.jpeg' },
+{ name: 'X-Salada', price: 30, vegan: false, src: './Assets/xsalada.jpeg' },
 
-{ name: 'X-Bacon', price: 34, vegan: false, src: './assets/xbacon.png' },
+{ name: 'X-Bacon', price: 34, vegan: false, src: './Assets/xbacon.png' },
 
-{ name: 'X-Bacon Egg', price: 39, vegan: false, src: './assets/bacon-egg.png' },
+{ name: 'X-Bacon Egg', price: 39, vegan: false, src: './Assets/bacon-egg.png' },
 
-{ name: 'Monstruoso', price: 50, vegan: false, src: './assets/monstruoso.png' },
+{ name: 'Monstruoso', price: 50, vegan: false, src: './Assets/monstruoso.png' },
 
-{ name: 'Big Vegano', price: 55, vegan: true, src: './assets/xvegan.png' },
+{ name: 'Big Vegano', price: 55, vegan: true, src: './Assets/xvegan.png' },
 
-{ name: 'X-Vegan', price: 45, vegan: true, src: './assets/monstruoso-vegan.png' },
+{ name: 'X-Vegan', price: 45, vegan: true, src: './Assets/monstruoso-vegan.png' },
 
 ]
